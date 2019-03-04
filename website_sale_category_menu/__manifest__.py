7d9fb5078ef19cacff91458f2af27766c3900876
Oriana Maita <o.maita@antilhue.com>
2019-03-04 16:16:47 -0300

    [IMP] website_sale_category: add category menu (#6)
    * [IMP] website_sale_category: add category menu
    
    * [OP] .circleci: test module
    
    * [REF] website_sale_category_menu: add README
    
    * [FIX][OP] .circleci: only run test our modules

---
+++ website_sale_category_menu/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Category Menu',
+    'category': 'Website',
+    'version': '1.0.0',
+    'depends': ['website_sale'],
+    'data': [
+        'views/assets.xml',
+        'views/templates.xml',
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'license': 'OPL-1',
+    'author': "Antilhue",
+}
