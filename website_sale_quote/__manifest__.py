962e72184982e35c16cf7d78f4dbae3ae7b870b5
Oriana Maita <maitaoriana@gmail.com>
2019-04-23 13:19:07 -0400

    [IMP] Welcome website sale quote
    - Cliente ahora puede genera una cotizacion de su carro de compras.
    - [OP] Especificar version de algoliasearch porque el modulo no es compatible con la version 2.0
    - [OP] Test website_sale_quote

---
+++ website_sale_quote/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Quote in e-commerce",
+    "summary": "This module allows the user to make a quote from the e-commerce",
+    "version": "11.0.0.0.1",
+    "category": "Website",
+    "license": "AGPL-3",
+    "depends": [
+        "website_sale",
+    ],
+    "external_dependencies": {
+    },
+    "data": [
+        'views/website_sale_template.xml'
+    ],
+    'installable': True,
+}
