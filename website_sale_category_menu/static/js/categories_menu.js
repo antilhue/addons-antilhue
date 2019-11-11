e785d7b44cd21cc805a20d391aca0781ec35ac4f
Oriana Maita <o.maita@antilhue.com>
2019-11-11 16:18:00 -0300

    [FIX] website_sale_category_menu: fix error in modal, when select category

--- website_sale_category_menu/static/js/categories_menu.js
+++ website_sale_category_menu/static/js/categories_menu.js
@@ -2,11 +2,11 @@ odoo.define('website_sale_category_manu.categories_menu', function (require) {
     "use strict";
 
     require('web.dom_ready');
-
-    $('.categories .nav').hover(function(){
-        let a = $('.container-opacacity');
-        a.toggleClass('container-show');
-        console.log(a)
-    });
-
+    $('.categories .nav')
+        .mouseenter(function() {
+          $('.container-opacacity').addClass('container-show');
+        })
+        .mouseleave(function() {
+            $('.container-opacacity').removeClass('container-show')
+        });
 });
