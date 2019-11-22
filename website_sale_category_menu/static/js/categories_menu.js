66486f802243113e2eac0b182d9471bb50e1823c
Oriana Maita <o.maita@antilhue.com>
2019-11-22 14:53:33 -0300

    [REF] website_sale_category: delay time added in hover to show categories only when user is placed in the category

--- website_sale_category_menu/static/js/categories_menu.js
+++ website_sale_category_menu/static/js/categories_menu.js
@@ -2,11 +2,19 @@ odoo.define('website_sale_category_manu.categories_menu', function (require) {
     "use strict";
 
     require('web.dom_ready');
-    $('.categories .nav')
-        .mouseenter(function() {
-          $('.container-opacacity').addClass('container-show');
-        })
-        .mouseleave(function() {
+    $('.mega-dropdown').hover(
+        function (){
+            const $category = $(this)
+            window.mytimeout = setTimeout(function() {
+                $('.container-opacacity').addClass('container-show');
+                $category.find('.mega-dropdown-menu').addClass('category-show')
+               console.log($category)
+               } , 250);
+        },
+        function () {
+            const $category = $(this)
+            $category.find('.mega-dropdown-menu').removeClass('category-show')
             $('.container-opacacity').removeClass('container-show')
-        });
+            clearTimeout(window.mytimeout);
+    });
 });
