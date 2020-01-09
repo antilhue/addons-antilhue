44bc725b483c8879c66cfffd6a9fba0ee29f306c
Oriana Maita <o.maita@antilhue.com>
2020-01-09 16:24:58 -0300

    [MIG] aws_monitoring: Migrate to v12

--- aws_monitoring/__manifest__.py
+++ aws_monitoring/__manifest__.py
@@ -3,7 +3,7 @@
     "name": 'Aws monitoring',
     "summary": "Add monitoring endpoint '/health'",
     "author": 'maitaoriana',
-    "version": '1.0',
+    "version": '12.0.1.0.0',
     "depends": ['base', 'web'],
     "license": 'AGPL-3',
     "data": [],
