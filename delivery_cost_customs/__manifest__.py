d6678510e38f4589a8aad77605b971fea1b1ba9f
Oriana Maita <o.maita@antilhue.com>
2020-01-09 17:45:04 -0300

    [MIG] delivery_cost_customs: Migrate to v12.0 (#27)
    * [MIG] delivery_cost_customs: Migrate to v12.0
    
    * [FIX] fix test, change country partner
    
    * [REF] translate
    
    * [REF] auth_signup_approval: translate module v12

--- delivery_cost_customs/__manifest__.py
+++ delivery_cost_customs/__manifest__.py
@@ -3,7 +3,7 @@
     "name": 'Customs cost in delivery',
     "summary": 'Include customs cost in delivery',
     "author": 'maitaoriana',
-    "version": '1.0',
+    "version": '12.0.1.0.0',
     "depends": ['delivery'],
     "license": "LGPL-3",
     "data": [
