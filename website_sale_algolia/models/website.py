3b5d77ca2ec5e7344f1fee17cd103ec4e9255b81
Oriana Maita <o.maita@antilhue.com>
2020-01-13 10:17:26 -0300

    [REM] website_sale_algolia: remove module website_sale_algolia (#31)

--- website_sale_algolia/models/website.py
+++
@@ -1,8 +0,0 @@
-from odoo import api, fields, models
-
-
-class Website(models.Model):
-    _inherit = 'website'
-
-    service_algolia_id = fields.Many2one(
-        'service.algolia', string="Server Algolia")
