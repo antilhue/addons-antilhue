c8755d190c3866c76f3c78b55fe831360f471973
Oriana Maita <o.maita@antilhue.com>
2020-01-14 13:37:46 -0300

    [REF] website_sale_quote: Migrate to v12  (#33)
    * [REF] website_sale_quote: Migrate to v12
    Ahora cuando cotiza no va a la vista cotizacion sino a la cotizacion en el portal.
    
    * [REF] .circleci: test website_sale_quote

--- website_sale_quote/controllers/main.py
+++ website_sale_quote/controllers/main.py
@@ -8,31 +8,18 @@ class WebsiteSaleQuote(WebsiteSale):
     @http.route(['/shop/quote'], type='http', auth="public", website=True)
     def quote(self, **post):
         order = request.website.sale_get_order()
-        if order.force_quotation_send():
+        if order and order.force_quotation_send():
             request.website.sale_reset()
-            return request.redirect('/shop/confirmation')
+            return request.redirect(order.get_portal_url())
 
         return request.render(
             'website_sale_quote.confirmation_order_error')
-
-    @http.route()
-    def payment_get_status(self, sale_order_id, **post):
-        order = request.env['sale.order'].sudo().browse(sale_order_id).exists()
-        if order.payment_acquirer_id:
-            return super(WebsiteSaleQuote, self).payment_get_status(
-                sale_order_id, **post)
-
-        return {
-            'recall': True,
-            'message': request.env['ir.ui.view'].render_template("website_sale_quote.order_state_message", {
-                'order': order
-            })
-        }
 
     @http.route(['/shop/quote/redirect'], type='http', auth="public", website=True)
     def redirect_saleorder(self):
         sale_order_id = request.session.get('sale_last_order_id')
         if sale_order_id:
-            return request.redirect('/quote/%s' % sale_order_id)
+            order = request.env['sale.order'].sudo().browse(sale_order_id)
+            return request.redirect(order.get_portal_url())
         else:
             return request.redirect('/shop')
