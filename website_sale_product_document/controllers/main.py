b8fbeef01f95592ad35ba4261c7e6774d9e54617
Oriana Maita <o.maita@antilhue.com>
2020-06-02 16:24:18 -0400

    [REF] Welcome website_sale_product_document

---
+++ website_sale_product_document/controllers/main.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import base64
+from odoo import http
+from odoo.http import request
+from odoo.addons.website_sale.controllers.main import WebsiteSale
+
+import werkzeug
+
+
+class WebsiteSale(WebsiteSale):
+
+    @http.route(['/product/download/document',], type='http', auth='public')
+    def download_product_document(self, document_id):
+        document = request.env['product.document'].browse(int(document_id))
+        if not document:
+            return request.not_found()
+
+        status, headers, content = request.registry['ir.http'].binary_content(id=document.sudo().attachment_id, download=True)
+        if status == 304:
+            response = werkzeug.wrappers.Response(status=status, headers=headers)
+        elif status == 301:
+            return werkzeug.utils.redirect(content, code=301)
+        elif status != 200:
+            response = request.not_found()
+        else:
+            content_base64 = base64.b64decode(content)
+            headers.append(('Content-Length', len(content_base64)))
+            response = request.make_response(content_base64, headers)
+
+        return response
+
+
