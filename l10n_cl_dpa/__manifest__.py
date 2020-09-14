da81a9e0b40a929f746b1a6c57540af17d3184cf
Oriana Maita <o.maita@antilhue.com>
2020-09-14 17:13:50 -0300

    [MIG] l10n_cl_dpa: migrate to 13.0

--- l10n_cl_dpa/__manifest__.py
+++ l10n_cl_dpa/__manifest__.py
@@ -4,7 +4,7 @@
     'category': 'Localization/Chile',
     "summary": 'Chilean administrative division',
     "author": 'maitaoriana',
-    "version": '12.0.1.0.0',
+    "version": '13.0.1.0.0',
     "depends": ['base_address_city'],
     "license": "LGPL-3",
     "data": [
