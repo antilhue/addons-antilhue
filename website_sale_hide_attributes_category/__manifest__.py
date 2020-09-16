d3c63d10420c536fd2414bd7408d1c5ca196f079
Oriana Maita <o.maita@antilhue.com>
2020-09-16 16:05:04 -0300

    [MIG] Migrate website_sale_hide_attributes_category and remove website_sale_category_menu

--- website_sale_hide_attributes_category/__manifest__.py
+++ website_sale_hide_attributes_category/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Web Categories without attribute filters',
     'category': 'Website',
-    'version': '12.0.1.0.0',
+    'version': '13.0.1.0.0',
     'depends': ['website_sale'],
     'data': [
         'views/public_categories.xml',
