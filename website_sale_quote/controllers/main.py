43d5c431f8f2982b14e11c7035883000e075dbba
Oriana Maita <o.maita@antilhue.com>
2020-09-21 12:12:35 -0300

    [MIG] website_sale_quote: Migrate to v13.0 (#54)
    * [MIG] website_sale_quote: Migrate to v13.0
    
    * [REF] .circleci: test module
    
    * [FIX]

--- website_sale_quote/controllers/main.py
+++ website_sale_quote/controllers/main.py
@@ -1,5 +1,6 @@
 from odoo import http
 from odoo.http import request
+from odoo import SUPERUSER_ID
 from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 
@@ -8,12 +9,18 @@ class WebsiteSaleQuote(WebsiteSale):
     @http.route(['/shop/quote'], type='http', auth="public", website=True)
     def quote(self, **post):
         order = request.website.sale_get_order()
-        if order and order.force_quotation_send():
+        if order.env.su:
+            # sending mail in sudo was meant for it being sent from superuser
+            order = order.with_user(SUPERUSER_ID)
+        email_act = order.action_quotation_send()
+        if email_act and email_act.get('context'):
+            email_ctx = email_act.get('context', {})
+            order.with_context(email_ctx).message_post_with_template(
+                email_ctx.get('default_template_id'),composition_mode='comment',
+                email_layout_xmlid="mail.mail_notification_paynow")
             request.website.sale_reset()
             return request.redirect(order.get_portal_url())
-
-        return request.render(
-            'website_sale_quote.confirmation_order_error')
+        return request.render('website_sale_quote.confirmation_order_error')
 
     @http.route(['/shop/quote/redirect'], type='http', auth="public", website=True)
     def redirect_saleorder(self):
